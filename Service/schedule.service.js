2f52fb070be06d0591e6291a568e8fbfdad7608f
dheeraj <dheerajkg8931@gmail.com>
2023-12-23 11:39:44 +0530

    update status

--- Service/schedule.service.js
+++ Service/schedule.service.js
@@ -106,102 +106,48 @@ exports.Post_To_All_SocialMedia_Scheduling_Post = async (req, res) => {
 
 
 exports.Post_Scheduling = async (req, res) => {
-    if (req.body.Platform && req.body.Date && req.body.userid.match(/^[0-9a-fA-F]{24}$/) && req.body.Content && req.body.Brand) {
 
-        var userdata = await user.findById(req.body.userid, function (err, result) {
+    // Get current time
+    const currentTime = new Date();
 
-            if (err) {
-                console.log(err);
-                res.json({
-                    status: 0,
-                    msg: "check your Credentials or internal server error"
+    // Calculate 1 minute ago and 1 minute ahead
+    const oneMinuteAgo = new Date(currentTime.getTime() - 5 * 60 * 1000);
+    const oneMinuteAhead = new Date(currentTime.getTime() + 5 * 60 * 1000);
 
-                })
-
-
-            }
-
-        })
+    const updatedBrand = await Post.updateOne({ 
+        Status: "Scheduled", Scheduledat: { $gte: oneMinuteAgo, $lte: oneMinuteAhead } },
+        { $set: { Status: "Live", } })
+       console.log(updatedBrand);
 
 
-
-        // console.log(userdata);
-        // if (userdata) {
-
-        //     if (userdata.facebookid && userdata.facebooktoken && userdata.Instagramid && userdata.Instagramtoken) {
-                // console.log(photopath);
-
-                // var Instagramid=req.body.Instagramid;
-                var Content = req.body.Content;
+                // var Content = req.body.Content;
 
 
 
-                var currentPoststack = userdata.Posts;
+                // var currentPoststack = userdata.Posts;
 
-                var Content = req.body.Content;
-                var Image = 'http://localhost:4000/images/';
+                // var Content = req.body.Content;
+                // var Image = 'http://localhost:4000/images/';
 
-                var base = 'https://graph.facebook.com/'
+                // var base = 'https://graph.facebook.com/'
 
-                var post = new Post({
+                // var post = new Post({
 
-                    userid: req.body.userid,
-                    instapostid: "",
-                    facebookpostid: "",
-                    Createdat: req.body.Date,
-                    Scheduledat: Date(),
-                    Status: "Scheduled",
-                    Platform: req.body.Platform,
-                    Brand: req.body.Brand
+                //     userid: req.body.userid,
+                //     instapostid: "",
+                //     facebookpostid: "",
+                //     Createdat: req.body.Date,
+                //     Scheduledat: Date(),
+                //     Status: "Scheduled",
+                //     Platform: req.body.Platform,
+                //     Brand: req.body.Brand
 
-
-                });
-
-                var postsave = await post.save();
-
-                // currentPoststack.push(postsave._id);
-                // console.log(req.body.userid);
-
-                // await user.findByIdAndUpdate(req.body.userid, { "Posts": currentPoststack }, function (err, docs) {
-
-                //     if (err) {
-                //         console.log(err)
-                //     }
-                //     else {
-                //         console.log("User Post has been  Updated");
-                //     }
 
                 // });
 
-                // const date = new Date(req.body.Date).toJSON();
+                // // var postsave = await post.save();
 
-
-
-                // if (req.body.Platform.includes("Facebook")) {
-                //     localStorage.setItem("Facebook" + postsave._id, "Facebook Post Scheduled at " + date + "current timing" + new Date());
-                //     schedule.scheduleJob(date, async function () {
-                //         console.log("Facebook executing function has been called")
-
-                //         posttoFacebookScheduled(userdata.facebookid, Image, Content, userdata.facebooktoken, postsave._id)
-
-
-                //     })
-                // }
-
-                // if (req.body.Platform.includes("Instagram")) {
-
-
-                //     localStorage.setItem("Instagram" + postsave._id, "Instagram Post Scheduled at " + date + "current timing" + new Date());
-                //     const job = schedule.scheduleJob(date, function (y) {
-
-                //         console.log("Instagram Scheduled function is Executing now");
-                //         postToInstaScheduled(userdata.Instagramid, Content, Image, userdata.Instagramtoken, postsave._id);
-
-                //     });
-                // }
-
-
-
+                
 
                 res.json({
                     code: 1,
@@ -209,37 +155,9 @@ exports.Post_Scheduling = async (req, res) => {
                 });
 
 
-        //     }
-        //     else {
-        //         res.json({
-        //             status: 0,
-        //             msg: "you have not saved valid Credential saved in database"
-
-        //         })
-
-        //     }
-
-        // }
-        // else {
-
-        //     res.json({
-        //         status: 0,
-        //         msg: "check your Credential User does not exist"
-
-        //     })
-
-        // }
 
 
 
 
-    }
-    else {
-
-        res.json({
-
-            status: 0,
-            msg: "Send All Necessary and valid Details post"
-        })
-    }
+    
 }
